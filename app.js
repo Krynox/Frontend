69aa120b2042e644a27760ff541322d20347683f
Krynox <deridderjames@hotmail.com>
2014-12-24 01:20:37 +0100

    Passport implementatie

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var passport=require('passport');
+var expressSession = require('express-session');
+var LocalStrategy = require('passport-local').Strategy;
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var app = express();
+var User= require("./data/models/user");
+var bCrypt=require('bcryptjs');
+
+var connectDB=require("./data/connectDB.js");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,11 +25,98 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/', routes);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
+
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use('login', new LocalStrategy({
+        passReqToCallback : true
+    },
+    function(req, username, password, done) {
+        // check in mongo if a user with username exists or not
+        User.findOne({ 'username' :  username },
+            function(err, user) {
+                // In case of any error, return using the done method
+                if (err)
+                    return done(err);
+                // Username does not exist, log error & redirect back
+                if (!user){
+                    console.log('User Not Found with username '+username);
+                    return done(null, false,'User Not found.');
+                }
+                // User exists but wrong password, log the error
+                if (!isValidPassword(user, password)){
+                    console.log('Invalid Password');
+                    return done(null, false,
+                        'Invalid Password');
+                }
+                // User and password both match, return user from
+                // done method which will be treated like success
+                return done(null, user);
+            }
+        );
+    }));
+
+passport.use('signup', new LocalStrategy({
+        passReqToCallback : true
+    },
+    function(req, username, password, done) {
+        findOrCreateUser = function(){
+            // find a user in Mongo with provided username
+            User.findOne({'username':username},function(err, user) {
+                // In case of any error return
+                if (err){
+                    console.log('Error in SignUp: '+err);
+                    return done(err);
+                }
+                // already exists
+                if (user) {
+                    console.log('User already exists');
+                    return done(null, false,
+                       'User Already Exists');
+                } else {
+                    var newUser = new User();
+                    newUser.username = username;
+                    newUser.password = createHash(password);
+
+                    // save the user
+                    newUser.save(function(err) {
+                        if (err){
+                            console.log('Error in Saving user: '+err);
+                            throw err;
+                        }
+                        console.log('User Registration succesful');
+                        return done(null, newUser);
+                    });
+                }
+            });
+        };
+        // Delay the execution of findOrCreateUser and execute
+        // the method in the next tick of the event loop
+        process.nextTick(findOrCreateUser);
+    }
+));
+
+var isValidPassword = function(user, password){
+    return bCrypt.compareSync(password, user.password);
+}
+var createHash = function(password){
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+}
+
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
